805caaadadae9e5670f3a2b507d8589e2a4083ba
Camilo Giraldo <camilo0o0o@users.noreply.github.com>
2025-03-20 09:23:35 +0000

    fix path inconsistencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from '@/components/Navbar'
+import Navbar from '@/components/Navbar/index'
 import Footer from '@/components/Footer/Footer'
 import Card from '@/components/projectsCards/Card'
 
