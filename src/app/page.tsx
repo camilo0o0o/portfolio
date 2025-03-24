7163f37dfce54cafef49a609aedf437e80214aa1
Camilo Giraldo <camilo0o0o@users.noreply.github.com>
2025-03-24 18:27:40 +0000

    Fix unused card component import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Navbar from '@/components/Navbar/index'
 import Footer from '@/components/Footer/Footer'
-import Card from '@/components/projectsCards/Card'
 import PhysicsGrid from '@/components/projectsCards/PhysicsGrid';
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col">
+    <main className="min-h-screen">
       <Navbar />
       <div className="w-full h-screen">
-        <PhysicsGrid backgroundColor="#f7f7f7" objectColor="#C25421"/>
+        <PhysicsGrid/>
       </div>
       <Footer />
     </main>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,7 +4,7 @@ import Timezone from './Timezone'
 
 export default function Navbar() {
     return (
-        <header className="fixed top-0 left-0 right-0 z-50 w-full px-8 py-4 flex items-center justify-between bg-background-grey">
+        <header className="fixed top-0 left-0 right-0 z-50 w-full px-8 py-4 flex items-center justify-between">
             <Logo />
             <Navigation />
             <Timezone />
