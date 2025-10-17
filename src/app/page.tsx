8f6fe2f67c2336433a1bd3818832ef21156a65df
Camilo Giraldo <camilo0o0o@users.noreply.github.com>
2025-10-17 20:05:57 +0100

    Remove unnecesary components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
-import Navbar from '@/components/Navbar/index'
-import Footer from '@/components/Footer/Footer'
-import PhysicsGrid from '@/components/projectsCards/PhysicsGrid';
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <Navbar />
-      <div className="w-full h-screen">
-        <PhysicsGrid/>
+    <main className="min-h-screen flex items-center justify-center relative">
+      <div className="absolute top-8 left-8">
+        <Image 
+          src="/mi_nombre.svg" 
+          alt="Camilo Giraldo" 
+          width={200}
+          height={28}
+          className="max-h-[28px] w-auto"
+          priority
+        />
       </div>
-      <Footer />
+      <Image 
+        src="/images/projects/selfie.jpeg" 
+        alt="Selfie" 
+        width={800}
+        height={600}
+        className="max-w-[250px] max-h-full object-contain border-2 border-dark-grey"
+        priority
+      />
     </main>
   );
 }
